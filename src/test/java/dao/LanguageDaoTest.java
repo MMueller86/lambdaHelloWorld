74c51392269a23a654811838696d870f6c6da0f4
MMueller86 <michi01mueller@gmail.com>
2017-05-07 21:39:40 +0200

    switched from derby to hsql and cleaned tests

--- src/test/java/dao/LanguageDaoTest.java
+++ src/test/java/dao/LanguageDaoTest.java
@@ -6,7 +6,6 @@ import helloWorld.entity.LanguageEntity;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -21,10 +20,10 @@ import static org.junit.Assert.assertNotNull;
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"classpath:spring_test.xml"})
-@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
 public class LanguageDaoTest {
     @Autowired
     ILanguageDAO sut;
+
 
     @Test
     @Sql("classpath:insertLanguages.sql")
